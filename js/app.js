cbcc78e65de595b1235a6d911cc48cccb4bd3972
Devon Gillis <devongillis88@gmail.com>
2022-08-27 08:01:59 -0300

    added comments to the code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 document.querySelector('#nav-btn').addEventListener('click', () => menu());
 
+/**
+ * Controls the animation and position of the nav menu by adding and removing a class
+ * @method
+ */
 function menu(){
     const nav = document.querySelector('#nav');
     if(nav.classList.contains('transform-nav')){
