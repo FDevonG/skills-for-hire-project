f7e2b6b5d766d21a36eb94e230599949b90d11d6
Devon Gillis <devongillis88@gmail.com>
2022-08-28 10:27:56 -0300

    fixed the navigation up so its more semantic, as well as added focus elements to the nvigation and the menu buttons. Also made a SVG hamburg menu in illustrator and aadded some hover and focus effects to that as well

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 document.querySelector('#nav-btn').addEventListener('click', () => menu());
+document.querySelector('#nav').setAttribute('display', 'none');
 
 /**
  * Controls the animation and position of the nav menu by adding and removing a class
@@ -8,7 +9,38 @@ function menu(){
     const nav = document.querySelector('#nav');
     if(nav.classList.contains('transform-nav')){
         nav.classList.remove('transform-nav');
-    } else {
+        window.onscroll = () => {};
+        navDisplay(false);
+;    } else {
         nav.classList.add('transform-nav');
+        disableScrolling();
+        navDisplay(true);
     }
+}
+
+/**
+ * 
+ * @param {Boolean} navSwitch - dictates wether we are hiding or showing the navigation
+ */
+function navDisplay(navSwitch) {
+    if(navSwitch){
+        setTimeout(() => {
+            document.querySelector('#nav').setAttribute('display', 'initial');;
+        }, 6000);
+    } else {
+        setTimeout(() => {
+            document.querySelector('#nav').setAttribute('display', 'none');;
+        }, 6000);
+    }
+}
+
+/**
+ * This disables scrolling
+ */
+ function disableScrolling(){
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
 }
